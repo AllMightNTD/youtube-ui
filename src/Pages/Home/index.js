3e3149ab49f9049ee08abc0d65b6a3956f7a330b
dungnhinho <dungnguyentien140602@gmail.com>
2022-08-06 23:30:01 +0700

    SideBarFull

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,76 @@
-function Home() {
-    return <h2>Home page</h2>;
+import React from 'react';
+// import Slider from 'react-slick';
+// // Import css files
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+import styles from './Home.module.scss';
+import classNames from 'classnames/bind';
+import 'tippy.js/dist/tippy.css'; // optional
+
+function SimpleSlider() {
+    var DataActions = [
+        {
+            action: 'Tất cả',
+            content: 'Tất cả',
+        },
+        {
+            action: 'Danh sách kết hợp',
+            content: 'Danh sách kết hợp',
+        },
+        {
+            action: 'Âm nhạc',
+            content: 'Âm nhạc',
+        },
+        {
+            action: 'Trò chơi',
+            content: 'Trò chơi',
+        },
+        {
+            action: 'Trực tiếp',
+            content: 'Trực tiếp',
+        },
+        {
+            action: 'Hài kịch tình huống',
+            content: 'Hài kịch tình huống',
+        },
+        {
+            action: 'Bóng đá',
+            content: 'Bóng đá',
+        },
+        {
+            action: 'Phim hoạt hình',
+            content: 'Phim hoạt hình',
+        },
+        {
+            action: 'Thiên nhiên',
+            content: 'Thiên nhiên',
+        },
+        {
+            action: 'Mới tải lên gần đây',
+            content: 'Mới tải lên gần đây',
+        },
+        {
+            action: 'Đã xem ',
+            content: 'Đã xem',
+        },
+        {
+            action: 'Đề xuất mới',
+            content: 'Đề xuất mới',
+        },
+    ];
+    const cx = classNames.bind(styles);
+
+    return (
+        <div className={cx('container')}>
+            <aside className={cx('header')}>
+                {DataActions.map((item, index) => (
+                    <button key={index} className={cx('action-item')}>
+                        {item.content}
+                    </button>
+                ))}
+            </aside>
+        </div>
+    );
 }
 
-export default Home;
+export default SimpleSlider;
--- src/components/Layout/Components/SideBar/Sidebar.js
+++ src/components/Layout/Components/SideBar/Sidebar.js
@@ -10,8 +10,17 @@ import {
     LikeIcon,
     SportsIcon,
     GameIcon,
+    CreatorStudioIcon,
+    YoutubeMusicICon,
+    YoutubeKidIcon,
+    YoutubeTvIcon,
+    SettingIcon,
+    HistoryReportIcon,
+    HelpIcon,
+    FeedBackIcon,
 } from '~/components/Icon';
 import config from '~/config';
+import { Link } from 'react-router-dom';
 import styles from './SideBar.module.scss';
 import classNames from 'classnames/bind';
 import Menu, { MenuItem } from './Menu';
@@ -53,7 +62,7 @@ function Sidebar() {
                         check
                         to={config.routes.watched}
                         src="https://yt3.ggpht.com/ytc/AMLnZu_5e_MCPIF4pbx2t0BfeC3uN3DfZYDQHIiKxdO1mw=s88-c-k-c0x00ffffff-no-rj
-                        "
+                            "
                         alt="F8 official"
                     />
                 </div>
@@ -61,6 +70,61 @@ function Sidebar() {
                     <h3 className={cx('title')}>KHÁM PHÁ </h3>
                     <MenuItem title="Trò chơi" to={config.routes.sports} icon={<SportsIcon />} />
                     <MenuItem title="Thể thao" to={config.routes.game} icon={<GameIcon />} />
+                </div>
+                <div className={cx('group-one')}>
+                    <h3 className={cx('title')}>DỊCH VỤ KHÁC CỦA YOUTUBE </h3>
+                    <MenuItem title="Creator Studio" to={config.routes.sports} icon={<CreatorStudioIcon />} />
+                    <MenuItem title="Youtube Music" to={config.routes.game} icon={<YoutubeMusicICon />} />
+                    <MenuItem title="Youtube Kids" to={config.routes.game} icon={<YoutubeKidIcon />} />
+                    <MenuItem title="Youtube TV" to={config.routes.game} icon={<YoutubeTvIcon />} />
+                </div>
+                <div className={cx('group-one')}>
+                    <MenuItem title="Cài đặt" to={config.routes.sports} icon={<SettingIcon />} />
+                    <MenuItem title="Lịch sử báo cáo" to={config.routes.game} icon={<HistoryReportIcon />} />
+                    <MenuItem title="Trợ giúp" to={config.routes.game} icon={<HelpIcon />} />
+                    <MenuItem title="Phản hồi" to={config.routes.game} icon={<FeedBackIcon />} />
+                </div>
+                <div className={cx('group-one')}>
+                    <div className={cx('group_footer')}>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Giới thiệu
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Báo chí
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Bản quyền
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Liên hệ với chúng tôi
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Người sáng tạo
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Quảng cáo
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Nhà phát triển
+                        </Link>
+                    </div>
+                    <div className={cx('group_footer')}>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Điều khiển
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Quyền riêng tư
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Chính sách và an toàn
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Cách Youtube hoạt động
+                        </Link>
+                        <Link to={'/following'} className={cx('footer_sidebar')}>
+                            Thử các tính năng mới
+                        </Link>
+                    </div>
                 </div>
             </Menu>
         </aside>
